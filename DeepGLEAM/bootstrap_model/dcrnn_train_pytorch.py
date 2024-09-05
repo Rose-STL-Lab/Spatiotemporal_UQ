7005d190a0533138b3934cf512862c44274109b9
Dongxia Wu <36138851+DongxiaW@users.noreply.github.com>
2024-09-05 13:36:30 -0700

    fix bootstrap issue

--- DeepGLEAM/bootstrap_model/dcrnn_train_pytorch.py
+++ DeepGLEAM/bootstrap_model/dcrnn_train_pytorch.py
@@ -9,10 +9,24 @@ import random
 from lib.utils import load_graph_data
 from model.pytorch.dcrnn_supervisor import DCRNNSupervisor
 
+import numpy as np
+import os
 
 def main(args):
     with open(args.config_filename) as f:
         supervisor_config = yaml.load(f)
+
+        data_path = os.path.join(supervisor_config['data'].get('dataset_dir'), 'train.npz')
+        data = np.load(data_path)
+        for i in range (1):
+            index = np.random.choice(25, 23, replace=True)
+            np.savez_compressed(
+                        os.path.join(supervisor_config['data'].get('dataset_dir'), "train_"+str(i)+".npz"),
+                        x=data['x'][index],
+                        y=data['y'][index],
+                        x_offsets=data['x_offsets'],
+                        y_offsets=data['y_offsets'],
+                    )
 
         graph_pkl_filename = supervisor_config['data'].get('graph_pkl_filename')
         sensor_ids, sensor_id_to_ind, adj_mx = load_graph_data(graph_pkl_filename)
